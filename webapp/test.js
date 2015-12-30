49ef144f76b0861167eee3910d415a2caeb1d2ad
Andy Tran <tran_fong1991@yahoo.com>
2015-12-29 22:13:54 -0600

    modified test.js

--- webapp/test.js
+++ webapp/test.js
@@ -1,6 +1,1 @@
-var http = require('http');
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello Travis!\n');
-}).listen(3000, '127.0.0.1');
-console.log('Server running at http://localhost:3000/');
+console.log("Hello Travis from test.js");
