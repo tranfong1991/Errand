23ca3654af00027eb877c686d7e6304aa24fca57
Andy Tran <tran_fong1991@yahoo.com>
2016-01-04 23:17:10 +0000

    working errand template layout

--- webapp/client/js/controllers/errand-controller.js
+++ webapp/client/js/controllers/errand-controller.js
@@ -4,7 +4,10 @@ app.controller('errandController', ['$scope', 'Errand', '$http', function($scope
 
     $scope.pageChanged = function(){
 	//use get() instead of query() because query() expects an array, but result is an object
-	Errand.get({page: $scope.currentPage}, function(result){
+	Errand.get({
+	    page: $scope.currentPage,
+	    location: $scope.location.city
+	}, function(result){
 	    $scope.errands = result.docs;
 
 	    //setup pagination information
@@ -15,20 +18,21 @@ app.controller('errandController', ['$scope', 'Errand', '$http', function($scope
 	});
     };
 
-    //call when the first time the page load to retrieve errands from server
-    $scope.pageChanged();
-
     $scope.getLocation = function(){
 	//ip-api is used to get location given an IP address
 	$http.get('http://ip-api.com/json/' + ip)
 	.then(function(res){
-	    if(res.data.status === 'success')
+	    if(res.data.status === 'success'){
 		$scope.location = res.data;
+
+		//only called when location is known
+		$scope.pageChanged();
+	    }
 	    else $scope.location = "unknown";
 	});
     }
     
-    //call when the page finished loading to retrieve location based on client IP.
+    //called when the page finished loading to retrieve location based on client IP.
     $scope.getLocation();
 
     $scope.login = function(){
--- webapp/server/controllers/errand-controller.js
+++ webapp/server/controllers/errand-controller.js
@@ -2,101 +2,100 @@ var Errand = require('../models/errand');
 var utils = require('../utils');
 
 module.exports = {
-	create : function(req, res){
-		var errand = new Errand(req.body);
+    create : function(req, res){
+	var errand = new Errand(req.body);
 
-		errand.save(function(err){
-			if(err)
-			utils.handleCreateError(res);
-			else utils.handleCreateSuccess(res);
-		});
+	errand.save(function(err){
+	    if(err)
+		utils.handleCreateError(res);
+	    else utils.handleCreateSuccess(res);
+	});
+    },
+
+    listAll : function(req, res){
+	Errand.paginate({}, {
+	    page: (req.query.page ? req.query.page : 1),
+	    limit: (req.query.limit ? req.query.limit : 10),
+	    select: req.query.fields,
+	    populate: {
+		path: 'customer runner',	//populate customer and runner fields. Separated by space
+		select: 'name profile_pic_url'	//only populate name and profilePicUrl of the above 2 fields. Also separated by space
+	    }
 	},
+			function(err, result){
+			    if(result == null)
+				utils.handleNullResult(res);
+			    else res.json(result);
+			});
+    },
 
-	listAll : function(req, res){
-		Errand.paginate({}, {
-			page: (req.query.page ? req.query.page : 1),
-			limit: (req.query.limit ? req.query.limit : 10),
-			select: req.query.fields,
-			populate: {
-				path: 'customer runner',	//populate customer and runner fields. Separated by space
-				select: 'name profile_pic_url'	//only populate name and profilePicUrl of the above 2 fields. Also separated by space
-			}
-		},
-		function(err, result){
-			if(result == null)
-			utils.handleNullResult(res);
-			else res.json(result);
-		});
-	},
+    listOne : function(req, res){
+	Errand.findById(req.params.id)
+	    .select(req.query.fields)
+	    .populate({
+		path: 'customer runner',
+		select: 'name profile_pic_url'
+	    }).exec(function(err, result){
+		if(result == null)
+		    utils.handleNullResult(res);
+		else res.json(result);
+	    });
+    },
 
-	listOne : function(req, res){
-		Errand.findById(req.params.id)
-		.select(req.query.fields)
-		.populate({
-			path: 'customer runner',
-			select: 'name profile_pic_url'
-		}).exec(function(err, result){
-			if(result == null)
-			utils.handleNullResult(res);
-			else res.json(result);
-		});
-	},
+    search : function(req, res){
+	Errand.find({
+	    $text:{
+		$search: req.query.term
+	    }
+	}).exec(function(err, result){
+	    if(err)
+		utils.handleNullResult(res);
+	    res.json(result);
+	});
+    },
 
-	search : function(req, res){
-		Errand.find({
-			$text:{
-				$search: req.query.term
-			}
-		})
-		.exec(function(err, result){
-		    if(err)
-			utils.handleNullResult(res);
-		    res.json(result);
-		});
-	},
+    remove : function(req, res){
+	Errand.remove({_id:req.params.id}, function(err){
+	    if(err)
+		utils.handleRemoveError(res);
+	    else utils.handleSuccess(res);
+	});
+    },
 
-	remove : function(req, res){
-		Errand.remove({_id:req.params.id}, function(err){
-			if(err)
-			utils.handleRemoveError(res);
-			else utils.handleSuccess(res);
-		});
-	},
+    update : function(req, res){
+	//$set operator is used to modify some of the field values
+	Errand.update({_id: req.params.id}, {$set: req.body}, function(err){
+	    if(err)
+		utils.handleUpdateError(res);
+	    else utils.handleSuccess(res);
+	});
+    },
 
-	update : function(req, res){
-		//$set operator is used to modify some of the field values
-		Errand.update({_id: req.params.id}, {$set: req.body}, function(err){
-			if(err)
-			utils.handleUpdateError(res);
-			else utils.handleSuccess(res);
-		});
-	},
+    //called when a user takes an errand
+    take : function(req, res){
+	Errand.update({_id: req.params.id}, {
+	    $set: {
+		is_taken: true,
+		runner: req.query.user	//no need to cast to ObjectId, it is automatically cast
+	    }
+	}, function(err){
+	    if(err)
+		utils.handleUpdateError(res);
+	    else utils.handleSuccess(res);
+	});
+    },
 
-	//called when a user takes an errand
-	take : function(req, res){
-		Errand.update({_id: req.params.id}, {
-			$set: {
-				is_taken: true,
-				runner: req.query.user	//no need to cast to ObjectId, it is automatically cast
-			}
-		}, function(err){
-			if(err)
-			utils.handleUpdateError(res);
-			else utils.handleSuccess(res);
-		});
-	},
-
-	//called when a user untake an errand
-	untake : function(req, res){
-		Errand.update({_id: req.params.id}, {
-			$set: {
-				is_taken: false,
-				runner: null
-			}
-		}, function(err){
-			if(err)
-			utils.handleRemoveError(res);
-			else utils.handleSuccess(res);
-		});
-	}
+    //called when a user untake an errand
+    untake : function(req, res){
+	Errand.update({_id: req.params.id}, {
+	    $set: {
+		is_taken: false,
+		runner: null
+	    }
+	}, function(err){
+	    if(err)
+		utils.handleRemoveError(res);
+	    else utils.handleSuccess(res);
+	});
+    }
 }
